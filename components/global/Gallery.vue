2e3d230d543d63fbd9f47dec20b928c39151e02c
Luca Engelhard <me@lucaengelhard.com>
2024-02-05 00:49:57 +0100

    gallery

---
+++ components/global/Gallery.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="gallery">
+        <slot />
+    </div>
+
+    <!-- <img v-for="image in images" src="image" alt="">-->
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.gallery {
+    display: block;
+}
+</style>

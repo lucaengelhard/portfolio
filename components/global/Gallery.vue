25d9adf022f6c3545f170030944e675f4a42933d
Luca Engelhard <me@lucaengelhard.com>
2024-02-05 02:12:24 +0100

    gallery

--- components/global/Gallery.vue
+++ components/global/Gallery.vue
@@ -1,17 +1,56 @@
 <template>
-    <div class="gallery">
+    <div ref="gallery" class="gallery">
         <slot />
+        <GalleryCounter />
     </div>
-
-    <!-- <img v-for="image in images" src="image" alt="">-->
 </template>
 
 <script setup>
+const gallery = ref(null)
+const length = useState("length")
+const current = useState("current")
 
+callOnce(() => {
+    length.value = 1
+    current.value = 1
+})
+
+onMounted(() => {
+    const images = Array.from(gallery.value.children[0].children);
+    length.value = images.length;
+
+    const options = {
+        root: gallery.value.children[0],
+        rootMargin: '10%',
+        threshold: 1
+    }
+
+    function callback(entries, observer) {
+        for (let i = 0; i < entries.length; i++) {
+            const entry = entries[i];
+            if (entry.isIntersecting) {
+                current.value = parseInt(entry.target.dataset.index) + 1;
+            }
+        }
+    }
+
+    let observer = new IntersectionObserver(callback, options);
+
+    for (let i = 0; i < Array.from(gallery.value.children[0].children).length; i++) {
+        const child = Array.from(gallery.value.children[0].children)[i];
+        child.dataset.index = i
+        observer.observe(child);
+    }
+
+
+
+
+})
 </script>
 
 <style scoped>
 .gallery {
     display: block;
+    position: relative;
 }
 </style>

31c0c82201d8917fd58d552ae0addafe3041f6cc
Luca Engelhard <me@lucaengelhard.com>
2024-02-12 15:39:08 +0100

    nuxtt-image implementation

--- components/global/LandingImage.vue
+++ components/global/LandingImage.vue
@@ -1,5 +1,5 @@
 <template>
-    <img :src="src" alt="" class="landing-bg" />
+    <NuxtImg loading="lazy" width="100vw" format="webp" quality="80" :src="src" alt="" class="landing-bg" />
 </template>
 
 <script setup>

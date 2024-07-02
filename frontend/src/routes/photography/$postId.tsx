2cbde9b8c2483dcc99bcb0e9500ad40ec03ed734
Luca Engelhard <me@lucaengelhard.com>
2024-07-02 16:12:55 +0200

    remove gallery pagination query

--- frontend/src/routes/photography/$postId.tsx
+++ frontend/src/routes/photography/$postId.tsx
@@ -17,7 +17,7 @@ const PROJECT = gql`
         attributes {
           Title
           Description
-          Gallery(pagination: { limit: 100 }) {
+          Gallery {
             Description
             Images {
               data {
